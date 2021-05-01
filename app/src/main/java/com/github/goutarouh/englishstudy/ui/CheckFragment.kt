11653bffa12a4de61120e03708395170978eb67e
goutarouh <gouta.gorogoro@gmail.com>
2021-05-01 13:31:40 +0900

    - ナビゲーションの下準備
    - gradleに依存を追加
    
    - 仮でCheckFragmentを追加
    
    - (警告を消すためにtestのversionを指定)

---
+++ app/src/main/java/com/github/goutarouh/englishstudy/ui/CheckFragment.kt
@@ -0,0 +1,18 @@
+package com.github.goutarouh.englishstudy.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.github.goutarouh.englishstudy.R
+
+class CheckFragment: Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_check, container, false)
+    }
+}
