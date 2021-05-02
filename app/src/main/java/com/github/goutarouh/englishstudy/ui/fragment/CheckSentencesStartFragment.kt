a331a7313787674626417525095dafcf15fae535
goutarouh <gouta.gorogoro@gmail.com>
2021-05-02 21:08:22 +0900

    - Fragmentの名前を変更
    - CheckSentencesFragment.ktからCheckSentencesStartFragment.ktに変更

--- app/src/main/java/com/github/goutarouh/englishstudy/ui/fragment/CheckSentencesStartFragment.kt
+++ app/src/main/java/com/github/goutarouh/englishstudy/ui/fragment/CheckSentencesStartFragment.kt
@@ -7,23 +7,23 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import com.github.goutarouh.englishstudy.databinding.FragmentCheckSentencesBinding
+import com.github.goutarouh.englishstudy.databinding.FragmentCheckSentencesStartBinding
 import com.github.goutarouh.englishstudy.viewmodel.CheckSentenceViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class CheckSentencesFragment: Fragment() {
+class CheckSentencesStartFragment: Fragment() {
 
     val viewModel: CheckSentenceViewModel by viewModels()
 
-    private lateinit var binding: FragmentCheckSentencesBinding
+    private lateinit var binding: FragmentCheckSentencesStartBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = FragmentCheckSentencesBinding.inflate(inflater, container, false)
+        binding = FragmentCheckSentencesStartBinding.inflate(inflater, container, false)
         return binding.root
     }
 
