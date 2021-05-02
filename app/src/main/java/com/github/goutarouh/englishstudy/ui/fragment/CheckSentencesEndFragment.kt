72f3040a0bd3447ca206ca1599033dc673cf3e5e
goutarouh <gouta.gorogoro@gmail.com>
2021-05-02 21:29:48 +0900

    - 確認テストの一連の流れのフラグメントを作成
    - 各フラグメントをnavigationで接続しました。
    
    - 各フラグメントにviewModelを追加

--- app/src/main/java/com/github/goutarouh/englishstudy/ui/fragment/CheckSentencesEndFragment.kt
+++ app/src/main/java/com/github/goutarouh/englishstudy/ui/fragment/CheckSentencesEndFragment.kt
@@ -9,31 +9,32 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.github.goutarouh.englishstudy.R
-import com.github.goutarouh.englishstudy.databinding.FragmentCheckSentencesStartBinding
+import com.github.goutarouh.englishstudy.databinding.FragmentCheckSentencesEndBinding
 import com.github.goutarouh.englishstudy.viewmodel.CheckSentenceViewModel
-import dagger.hilt.android.AndroidEntryPoint
 
-@AndroidEntryPoint
-class CheckSentencesStartFragment: Fragment() {
+/**
+ * 問題後のフラグメント
+ */
+class CheckSentencesEndFragment: Fragment() {
+
+    lateinit var binding: FragmentCheckSentencesEndBinding
 
     val viewModel: CheckSentenceViewModel by viewModels()
-
-    private lateinit var binding: FragmentCheckSentencesStartBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = FragmentCheckSentencesStartBinding.inflate(inflater, container, false)
+        binding = FragmentCheckSentencesEndBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        binding.checkStart.setOnClickListener {
-            findNavController().navigate(R.id.action_bottom_navigation_check_to_checkSentencesFragment)
+        binding.checkEnd.setOnClickListener {
+            findNavController().navigate(R.id.action_checkSentencesEndFragment_to_bottom_navigation_check)
         }
     }
+
 }
--- app/src/main/java/com/github/goutarouh/englishstudy/ui/fragment/CheckSentencesFragment.kt
+++ app/src/main/java/com/github/goutarouh/englishstudy/ui/fragment/CheckSentencesFragment.kt
@@ -9,31 +9,32 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.github.goutarouh.englishstudy.R
-import com.github.goutarouh.englishstudy.databinding.FragmentCheckSentencesStartBinding
+import com.github.goutarouh.englishstudy.databinding.FragmentCheckSentencesBinding
 import com.github.goutarouh.englishstudy.viewmodel.CheckSentenceViewModel
-import dagger.hilt.android.AndroidEntryPoint
 
-@AndroidEntryPoint
-class CheckSentencesStartFragment: Fragment() {
+/**
+ * 問題を表示する
+ */
+class CheckSentencesFragment: Fragment() {
+
+    lateinit var binding: FragmentCheckSentencesBinding
 
     val viewModel: CheckSentenceViewModel by viewModels()
-
-    private lateinit var binding: FragmentCheckSentencesStartBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = FragmentCheckSentencesStartBinding.inflate(inflater, container, false)
+        binding = FragmentCheckSentencesBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        binding.checkStart.setOnClickListener {
-            findNavController().navigate(R.id.action_bottom_navigation_check_to_checkSentencesFragment)
+        binding.checkNext.setOnClickListener {
+            findNavController().navigate(R.id.action_checkSentencesFragment_to_checkSentencesEndFragment)
         }
     }
+
 }
